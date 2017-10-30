da8c387f0242abe66dd8a36f8c15740abec38604
BRENDA-ACER\Brenda <brnjdb@gmail.com>
2017-10-30 12:37:13 +0700

    Adding addWish method

--- app/src/main/java/data/DatabaseHandler.java
+++ app/src/main/java/data/DatabaseHandler.java
@@ -1,8 +1,11 @@
 package data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import models.MyWish;
 
 /**
  * Created by Brenda on 30/10/2017.
@@ -24,5 +27,16 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists whishes");
         onCreate(db);
+    }
+
+    //menambahkan data di db wishes
+    public void addWish(MyWish wish){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues val = new ContentValues();
+        val.put("title", wish.getTitle());
+        val.put("content", wish.getContent());
+        val.put("recordDate", System.currentTimeMillis()); //tak coba tanpa java.lang
+        db.insert("wishes", null, val);
+        db.close();
     }
 }
